c01250becdec6b985afa4e2ca1f59888076979bc
fxckfallen <fxckfallen@icloud.com>
2026-06-09 20:29:12 +0300

    added huffman tree

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include <fstream>
 #include <cstdint>
 #include <vector>
+#include <queue>
+
+struct Node
+{
+    uint8_t byte;
+    int freq;
+    Node* left;
+    Node* right;
+};
+
 
 std::vector<uint8_t> read_bytes(char* filepath) {
     std::ifstream ifs;
@@ -33,15 +43,43 @@ std::vector<int> count_freq(std::vector<uint8_t> bytes) {
     return freq;
 }
 
+struct comparator {
+        bool operator()(Node* i, Node* j) {
+        return i->freq > j->freq;
+    }
+};
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " filename" << std::endl;
         return 1;
     }
+
     std::vector<uint8_t> bytes = read_bytes(argv[1]);
     std::vector<int> freq = count_freq(bytes);
 
-    for (int i = 0; i < 256; ++i) std::cout << static_cast<char>(i) << ": " << freq[i] << std::endl;
+    std::priority_queue<Node*, std::vector<Node*>, comparator> tree;
+
+    for (int i = 0; i < 256; ++i) {
+        if (freq[i] > 0) {
+            Node* node = new Node();
+            node->byte = static_cast<uint8_t>(i);
+            node->freq = freq[i];
+            tree.push(node);
+        }
+    }
+
+    while (tree.size() > 1) {
+        Node* left = tree.top();
+        tree.pop();
+        Node* right = tree.top();
+        tree.pop();
+        Node* root = new Node();
+        root->freq = right->freq + left->freq;
+        root->left = left;
+        root->right = right;
+        tree.push(root);
+    }
 
     return 0;
 }
